47a3e3844f9665542d83d6397117b696bd58951f
brahmkshatriya <69040506+brahmkshatriya@users.noreply.github.com>
2022-04-26 02:35:48 +0530

    Library - Added kotlinx.coroutine dependency - Add Async Functionality to Call - Updated target version to 32
    Example App
    - Added androidx.lifecycle dependency
    - Add comments to examples
    - Add Async Example

--- app/src/main/java/com/lagradost/nicehttp/example/MainActivity.kt
+++ app/src/main/java/com/lagradost/nicehttp/example/MainActivity.kt
@@ -2,8 +2,11 @@ package com.lagradost.nicehttp.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.lagradost.nicehttp.Requests
-import kotlin.concurrent.thread
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 data class GithubJson(
     val description: String,
@@ -16,15 +19,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        thread {
+        lifecycleScope.launch {
             val requests = Requests()
 
+            //Example for query selector
             val doc = requests.get("https://github.com/Blatzar/NiceHttp").document
             println(doc.select("p.f4.my-3").text())
 
+            //Example for json Parser
             val json =
                 requests.get("https://api.github.com/repos/blatzar/nicehttp").parsed<GithubJson>()
             println(json.description)
+
+            //Example for Async-ed Requests
+            (0..5).toList().asyncEach {
+                println("Entered Async")
+                println("Response ::: " + requests.get("https://github.com/").code)
+                println("Exit Async")
+            }
         }
     }
 }
+
